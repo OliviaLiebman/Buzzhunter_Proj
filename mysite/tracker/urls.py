5917c603fcecf57d83f804bcf5dcf4a37a3ca4c3
Ori Herrmann <orijoshuaherrmann@gmail.com>
2017-03-07 11:48:13 +0200

    connected html templates

--- mysite/tracker/urls.py
+++ mysite/tracker/urls.py
@@ -3,5 +3,15 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.blog, name='blog'),
+    url(r'^$', views.buzzlove, name='buzzLove'),
+    url(r'^$', views.creative, name='creative'),
     url(r'^$', views.index, name='index'),
+    url(r'^$', views.index_all, name='index_all'),
+    url(r'^$', views.index_branding, name='index_branding'),
+    url(r'^$', views.index_buzzLove, name='index_buzzLove'),
+    url(r'^$', views.index_confidential, name='index_confidential'),
+    url(r'^$', views.index_web, name='index_web'),
+    url(r'^$', views.portfolio, name='portfolio'),
+    url(r'^$', views.website, name='website'),
 ]
--- mysite/tracker/views.py
+++ mysite/tracker/views.py
@@ -1,5 +1,39 @@
 from django.http import HttpResponse
-# import mySqldb
+from django.shortcuts import render
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request,'index.html')
+
+def blog(request):
+    return render(request,'blog.html')
+
+def buzzlove(request):
+    return render(request,'buzzLove.html')
+
+def creative(request):
+    return render(request,'creative.html')
+
+def index_all(request):
+    return render(request,'index_all.html')
+
+def index_branding(request):
+    return render(request,'index_branding.html')
+
+def index_buzzLove(request):
+    return render(request,'index_buzzLove.html')
+
+def index_confidential(request):
+    return render(request,'index_confidential.html')
+
+def index_web(request):
+    return render(request,'index_web.html')
+
+def portfolio(request):
+    return render(request,'portfolio.html')
+
+def website(request):
+    return render(request,'website.html')
+
+
+
