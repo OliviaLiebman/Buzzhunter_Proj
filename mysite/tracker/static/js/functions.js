07b0b0f3674411ffc7edb49aaf0c38473d5da50c
Ori Herrmann <orijoshuaherrmann@gmail.com>
2017-03-09 14:25:33 +0200

    cookies added

--- mysite/tracker/static/js/functions.js
+++ mysite/tracker/static/js/functions.js
@@ -22,3 +22,8 @@ document.addEventListener('click', function(event) {
 TimeMe.callAfterTimeElapsedInSeconds(15, function(){
     console.log("The user has been using the page for 15 seconds! Let's prompt them with something.");
 });
+
+  function getIP(json) {
+    console.log("IP address: ", json.ip);
+    document.cookie = (json.ip); "expires=Thu, 18 Dec 2018 12:00:00 UTC";
+  }
