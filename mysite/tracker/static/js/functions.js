4e68dea962d0b0f076d3e23cf9b7ecb8441a1b44
Olivia Liebman <olivialiebman@yahoo.com>
2017-03-13 17:42:04 +0200

    working basic cookie functionality

--- mysite/tracker/static/js/functions.js
+++ mysite/tracker/static/js/functions.js
@@ -5,12 +5,24 @@
 // Tracker.start = (function () {
 //
 // })();
-
+var url='127.0.0.1:8500';
 
 document.addEventListener('click', function(event) {
    console.log("x:" + event.clientX + " y:" + event.clientY );
-    console.log(event.target.innerHTML);
-    console.log(str);
+   // console.log(event.target.innerHTML);
+   // console.log(str);
+
+   if (event.target.id == 'welcome'){
+
+      var a = document.getElementById('menu-item-75').getElementsByTagName('a')[0];  //will get the href of what is attached to this id
+       var link = '127.0.0.1:8500/tracker/portfolio/';
+       // a.href = link;
+       // window.location.href = '127.0.0.1:8500/tracker/portfolio/';
+       console.log(link);
+       console.log(a);
+       alert("buzz clicked with id = " + event.target.id);
+       // window.location.href='127.0.0.1:9000/tracker/portfolio/';
+   }
 
     if(event.target instanceof SVGElement) {
         var s = new XMLSerializer();
@@ -44,18 +56,57 @@ document.addEventListener('click', function(event) {
         return res.json();
     })
     .then(function(data){
-        alert(JSON.stringify(data))
+        // alert(JSON.stringify(data))
     })
 
 });
 
 
 
-TimeMe.callAfterTimeElapsedInSeconds(15, function(){
-    console.log("The user has been using the page for 15 seconds! Let's prompt them with something.");
+TimeMe.callAfterTimeElapsedInSeconds(1, function(){
+    console.log("The user has been using the page for 1 second. Let's prompt them with something.");
+
 });
+
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
 
   function getIP(json) {
     console.log("IP address: ", json.ip);
-    document.cookie = (json.ip); "expires=Thu, 18 Dec 2018 12:00:00 UTC";
+    // document.cookie = (json.ip); "expires=Thu, 18 Dec 2018 12:00:00 UTC";
+
+    setCookie('buzz_cookie', json.ip, 365);
+    console.log("inside cookie:" + json.ip);
+
   }
+
+  function getCookie(cname) {//pass in key
+    var name = cname + "=";
+    var ca = document.cookie.split(';'); //list of key-value pairs of cookies
+    for(var i = 0; i < ca.length; i++) { //loops through length of list
+        var c = ca[i];
+        while (c.charAt(0) == ' ') { //gets rid of leading white spcae
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length); //returns the value of the key-value pair
+        }
+    }
+    return "";
+}
+(function checkCookie() {
+    var user = getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+        user = prompt("Please enter your name:", "");
+        if (user != "" && user != null) {
+            setCookie("username", user, 365);
+        }
+    }
+})();
--- mysite/tracker/urls.py
+++ mysite/tracker/urls.py
@@ -8,16 +8,17 @@ router.register(r'api', user_interactionViewSet)
 
 urlpatterns = [
     url(r'^api/', include(router.urls)),
+   # url(r'^api$', include(router.urls)), changed to above line
     url(r'^api/index$', views.ApiIndexView.as_view()),
-    url(r'^$', views.website, name='website'),
-    url(r'^$', views.portfolio, name='portfolio'),
-    url(r'^$', views.index, name='index'),
-    url(r'^$', views.blog, name='blog'),
-    url(r'^$', views.buzzlove, name='buzzLove'),
-    url(r'^$', views.creative, name='creative'),
-    url(r'^$', views.index_all, name='index_all'),
-    url(r'^$', views.index_branding, name='index_branding'),
-    url(r'^$', views.index_buzzLove, name='index_buzzLove'),
-    url(r'^$', views.index_confidential, name='index_confidential'),
-    url(r'^$', views.index_web, name='index_web'),
+    url(r'^website/', views.website, name='website'),
+    url(r'^portfolio/', views.portfolio, name='portfolio'),
+    url(r'^index/', views.index, name='index'),
+    url(r'^blog/', views.blog, name='blog'),
+    url(r'^buzzlove/', views.buzzlove, name='buzzLove'),
+    url(r'^creative/', views.creative, name='creative'),
+    url(r'^index_all/', views.index_all, name='index_all'),
+    url(r'^index_branding/', views.index_branding, name='index_branding'),
+    url(r'^index_buzzlove/', views.index_buzzLove, name='index_buzzLove'),
+    url(r'^index_confidential/', views.index_confidential, name='index_confidential'),
+    url(r'^index_web/', views.index_web, name='index_web'),
 ]
