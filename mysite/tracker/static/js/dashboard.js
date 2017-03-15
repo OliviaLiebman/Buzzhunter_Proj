5ea66f603ede355ed19eedfa75ed3c28d00f6c1b
Olivia Liebman <olivialiebman@yahoo.com>
2017-03-15 15:17:13 +0200

    slightly working version

--- mysite/tracker/static/js/dashboard.js
+++ mysite/tracker/static/js/dashboard.js
@@ -6,35 +6,35 @@ console.log('i am in dashboard js');
 // var finalArray;
 /*var retVal = null;*/
 
-$(document).ready(function() {
-    /*function createTable(datagrabbed) {*/
+// $(document).ready(function() {
+    // function createTable(datagrabbed) {
+    //
+    //       finalArray = datagrabbed.map(function (obj) {
+    //           return [obj.buttons_clicked, obj.coordinates, obj.current_page];
+    //       });
 
-          // finalArray = datagrabbed.map(function (obj) {
-          //     return [obj.buttons_clicked, obj.coordinates, obj.current_page];
-          // });
-
-        /*console.log(datagrabbed);*/
+        // console.log(datagrabbed);
 
 
-        $('#example').DataTable({
-        //            "processing": true,
-        // "serverSide": true,
-            ajax: "tracker/api/index/",
-            // "type": "POST",
-            // data:datagrabbed,
-            // data: [{buttons_clicked:3, coordinates: 4, current_page:5}, {buttons_clicked:3, coordinates: 4, current_page:5}, {buttons_clicked:3, coordinates: 4, current_page:5}],
-            columns: [
-                { "data": "buttons_clicked" },
-                { "data": "coordinates" },
-                { "data": "current_page" },
-                { "data": "date_time_of_access" },
-                // { "data": "id" },
-                { "data": "overall_time" },
-                { "data": "page_visited" },
-                { "data": "user_id" }
-                 ]
-        });
-    });
+    //     $('#example').DataTable({
+    //     //            "processing": true,
+    //     // "serverSide": true,
+    //         ajax: "tracker/api/index/",
+    //         // "type": "POST",
+    //         // data:datagrabbed,
+    //         // data: [{buttons_clicked:3, coordinates: 4, current_page:5}, {buttons_clicked:3, coordinates: 4, current_page:5}, {buttons_clicked:3, coordinates: 4, current_page:5}],
+    //         columns: [
+    //             { "data": "buttons_clicked" },
+    //             { "data": "coordinates" },
+    //             { "data": "current_page" },
+    //             { "data": "date_time_of_access" },
+    //             // { "data": "id" },
+    //             { "data": "overall_time" },
+    //             { "data": "page_visited" },
+    //             { "data": "user_id" }
+    //              ]
+    //     });
+    // });
 
 
      // $.ajax({
@@ -48,24 +48,54 @@ $(document).ready(function() {
      //  }});
 
     // console.log(data);
-    // function createTable(datagrabbed) {
-    //
-    //     $('#example').DataTable({
-    //         data: datagrabbed,
-    //         columns: [
-    //             { data: "buttons_clicked" },
-    //             { data: "coordinates" },
-    //             { data: "current_page" },
-    //             { data: "date_time_of_access" },
-    //             { data: "id" },
-    //             { data: "overall_time" },
-    //             { data: "page_visited" },
-    //             { data: "user_id" }
-    //             ]
-    //     });
-    // }
+  //   function createTable(datagrabbed) {
+  //
+  //       $('#example').DataTable({
+  //           data: datagrabbed,
+  //           columns: [
+  //               { data: "buttons_clicked" },
+  //               { data: "coordinates" },
+  //               { data: "current_page" },
+  //               { data: "date_time_of_access" },
+  //               { data: "id" },
+  //               { data: "overall_time" },
+  //               { data: "page_visited" },
+  //               { data: "user_id" }
+  //               ]
+  //       });
+  //   }
+  //
+  // });
 
- /* });*/
+  function createTable(data) {
+
+        $('#example').DataTable({
+            data: data,
+            columns: [
+                { data: "buttons_clicked" },
+                { data: "coordinates" },
+                { data: "current_page" },
+                { data: "date_time_of_access" },
+                { data: "id" },
+                { data: "overall_time" },
+                { data: "page_visited" },
+                { data: "user_id" }
+                ]
+        });
+    }
+
+     $.ajax({
+            type: 'GET',
+            url: "http://127.0.0.1:8500/tracker/api/index/",
+      success: function(datagrabbed) {
+          console.log(datagrabbed);
+          var data = datagrabbed;
+          createTable(data);
+      }});
+
+
+
+
 
 
 
